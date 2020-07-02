80359699d2a945b028cb1064b948fce23a8a1fc9
Sharnell Grant <sharnellgrant@gmail.com>
2020-07-02 02:25:43 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,9 +12,9 @@ class AuthorsController < ApplicationController
   end  
 
   def create
-    @author = Author.create(author_params)
-      if @author.save
-        
+    @author = Author.new(author_params)
+      if @author.valid?
+        @author.save
         redirect_to author_path(@author)
       else
         render :new 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  
+  def new 
+    @post = Post.new
+  end  
 
+  def create
+    @post = Post.new(post_params)
+      if @post.valid?
+        @post.save
+        
+        redirect_to post_path(@post)
+      else
+        render :new 
+    end
+  end
+  
   def edit
     @post = Post.find(params[:id])
   end
